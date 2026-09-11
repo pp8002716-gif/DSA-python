b9afeb000f8f23134facf921abc5116f512dafaa
pp8002716-gif <pp8002716@gmail.com>
2026-09-11 23:40:19 +0530

    Initial commit

---
+++ Arrays/Remove_Duplicates.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        index = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[index] = nums[i]
+                index += 1
+
+        return index
